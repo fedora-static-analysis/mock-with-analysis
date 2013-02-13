800ee595735ff29d186c8552a08a21e6b127c0fd
David Malcolm <dmalcolm@redhat.com>
2013-02-13 17:02:52 -0500

    clean up reports/find-failures.py

--- reports/find-failures.py
+++ reports/find-failures.py
@@ -1,31 +1,43 @@
 import glob
 
 from reports import get_filename, ResultsDir, AnalysisIssue, Model, \
-    AnalysisFailure, Failure
+    AnalysisFailure, Failure, Issue
 
-for path in glob.glob('2013-02-12-fedora-17-mass-run/*/*/*/*/static-analysis'):
-    print(path)
-    rdir = ResultsDir(path)
-    model = Model(rdir)
-    for a in model.iter_analyses():
-        for result in a.results:
-            if isinstance(result, Failure):
-                print('%s:%s' % (a.metadata.generator.name, result.failureid))
-                if result.location:
-                    if result.location.function:
-                        print("  In function '%s':"
-                              % result.location.function.name)
+GLOBPATH='2013-02-12-fedora-17-mass-run/*/*/*/*/static-analysis'
 
-                if result.failureid == 'python-exception':
-                    print('    %s' % result.customfields['traceback'].splitlines()[-1])
+def iter_reports(globpath):
+    for path in glob.glob(globpath):
+        print(path)
+        rdir = ResultsDir(path)
+        model = Model(rdir)
+        for a in model.iter_analyses():
+            for result in a.results:
+                if isinstance(result, Issue):
+                    yield model, AnalysisIssue(a, result)
+                if isinstance(result, Failure):
+                    yield model, AnalysisFailure(a, result)
 
-                if result.location:
-                    if result.location.file:
-                        code = model.get_file_content(result.location.file)
-                        lines = code.splitlines()
-                        print('\n'.join(('    %05i%s| %s '
-                                         % (linenum,
-                                            '>' if linenum == result.location.line else ' ',
-                                            lines[linenum - 1]))
-                                        for linenum in range(max(0, result.location.line - 5),
-                                                             min(result.location.line + 6, len(lines)))))
+def show_all_failures(globpath):
+    for model, af in iter_reports(globpath):
+        if isinstance(af, AnalysisFailure):
+            print('%s:%s' % (af.generator.name, af.failureid))
+            if af.location:
+                if af.location.function:
+                    print("  In function '%s':"
+                          % af.location.function.name)
+
+            if af.failureid == 'python-exception':
+                print('    %s' % af.customfields['traceback'].splitlines()[-1])
+
+            if af.location:
+                if af.location.file:
+                    code = model.get_file_content(af.location.file)
+                    lines = code.splitlines()
+                    print('\n'.join(('    %05i%s| %s '
+                                     % (linenum,
+                                        '>' if linenum == af.location.line else ' ',
+                                        lines[linenum - 1]))
+                                    for linenum in range(max(0, af.location.line - 5),
+                                                         min(af.location.line + 6, len(lines)))))
+
+show_all_failures(GLOBPATH)
